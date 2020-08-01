9dfbfffbbafb3dcdc64df29fa7261aa0b08b8717
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-01 22:51:15 +0530

    added TextField to search.jsx

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { Component } from "react";
-import TextField from 'material-ui/TextField';
-import SelectField from 'material-ui/SelectField';
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
 
 class Search extends Component {
-state={
-    searchText:"",
-    amount:15,
-    apiUrl:"https://pixapbay.com/api",
-    apiKey:"17733946-da09621ea7c5db4c99e7c04dd",
-    images:[]
-}
+  state = {
+    searchText: "",
+    amount: 15,
+    apiUrl: "https://pixapbay.com/api",
+    apiKey: "17733946-da09621ea7c5db4c99e7c04dd",
+    images: [],
+  };
 
   render() {
-    return
-     <div>
-    
-         </div>;
+    return (
+      <div>
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.handleChange}
+          floatingLabelText="Search for the images"
+          fullWidth={true}
+        />
+      </div>
+    );
   }
 }
 
