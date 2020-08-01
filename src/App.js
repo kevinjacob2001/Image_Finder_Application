1715f138508baa024abaa0a2155db1f802b79e3a
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-01 22:18:16 +0530

    setupped the files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';````````````
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      
-
+Hello
 
     </div>
   );
