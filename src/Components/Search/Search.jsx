3158d3aeb778b75701575176df43c97762b2f481
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-01 22:48:22 +0530

    added the States to the Search.jsx component

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Component } from 'react';
+import React from "react";
+import { Component } from "react";
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
 
-
-class Search extends Component{
-    render(){
-return(
-   <div>
-    hello
-   </div>
-
-)
-    }
+class Search extends Component {
+state={
+    searchText:"",
+    amount:15,
+    apiUrl:"https://pixapbay.com/api",
+    apiKey:"17733946-da09621ea7c5db4c99e7c04dd",
+    images:[]
 }
 
+  render() {
+    return
+     <div>
+    
+         </div>;
+  }
+}
 
 export default Search;
