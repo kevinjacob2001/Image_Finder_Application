4d8c0d6f9b596a76dd9a68a9b688235fe2c8168e
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-02 11:52:27 +0530

    Added all the imports to ImageFinder.jsx

--- src/Components/ImageResult/ImageResult.jsx
+++ src/Components/ImageResult/ImageResult.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import {GridList,GridTitle} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import ZoomIn from 'material-ui/svg-icon/action/zoom-in';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 class ImageResult extends Component{
     render(){
         return(
-Image 
+<div></div>
         )
     }
+}
+
+ImageResult.propTypes={
+    images:PropTypes.array.isRequired
 }
 
 export default ImageResult;
