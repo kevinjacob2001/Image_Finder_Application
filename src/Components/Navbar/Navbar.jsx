78c4d0f2b88254fdbe19864cef505597fc034e4d
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-02 14:48:20 +0530

    Link addition completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import App from '../../App';
 function Navbar(){
     return(
 <AppBar style={{background:"#061238",textAlign:"center"}}  title="Meraki!📷🔍">
-<a href="" >
+<a href="https://kevinjacob2001.github.io/" style={{color:"white",textDecoration:"none",marginTop:"20px"}}>
  Meet me!
   </a>
 </AppBar>
