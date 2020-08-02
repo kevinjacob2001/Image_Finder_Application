742fa6c821030eb08141c6b90fcdc7229025284d
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-02 15:12:15 +0530

    Updates

---
+++ precache-manifest.28bc0a2d70cb149d9584826f1eea62f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc00269b86daf9dc73b09e5a1137d38",
+    "url": "/Image_Finder_Application/index.html"
+  },
+  {
+    "revision": "b231cae5100e59b92b6d",
+    "url": "/Image_Finder_Application/static/css/main.9b2a3041.chunk.css"
+  },
+  {
+    "revision": "ee4b0cc9c262a8db66bf",
+    "url": "/Image_Finder_Application/static/js/2.f76885aa.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/Image_Finder_Application/static/js/2.f76885aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b231cae5100e59b92b6d",
+    "url": "/Image_Finder_Application/static/js/main.8fb52d39.chunk.js"
+  },
+  {
+    "revision": "1c075f8964c95befcdd8",
+    "url": "/Image_Finder_Application/static/js/runtime-main.7adef70c.js"
+  }
+]);
