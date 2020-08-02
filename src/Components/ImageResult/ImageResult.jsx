3d72f1704617454c7557c4413c6ea3ed51c9f865
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-02 12:00:07 +0530

    added imageListContent to ImageResults.jsx

--- src/Components/ImageResult/ImageResult.jsx
+++ src/Components/ImageResult/ImageResult.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
-import { Component } from 'react';
-import PropTypes from 'prop-types';
-import {GridList,GridTitle} from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
-import ZoomIn from 'material-ui/svg-icon/action/zoom-in';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
+import React from "react";
+import { Component } from "react";
+import PropTypes from "prop-types";
+import { GridList, GridTitle } from "material-ui/GridList";
+import IconButton from "material-ui/IconButton";
+import ZoomIn from "material-ui/svg-icon/action/zoom-in";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import GridTile from "material-ui/GridList/GridTile";
 
-class ImageResult extends Component{
-    render(){
-        return(
-<div></div>
-        )
+class ImageResult extends Component {
+  render() {
+    let imageListContent;
+    const { images } = this.props;
+
+    if (images) {
+      imageListContent = (
+        <GridList cols={3}>
+          {images.map((img) => {
+            <GridTile
+            title={img.tags}
+            key={img.id}
+            subtitle={
+                <span>
+                    by <strong>
+                        {img.user}
+                    </strong>
+                </span>
+            }
+            actionIcon={
+                <IconButton>
+                    <ZoomIn color="white"/>
+                </IconButton>
+            }
+            >
+<img src={img.largeImageURL} alt=""/>
+            </GridTile>;
+          })}
+        </GridList>
+      );
+    } else {
+      imageListContent = null;
     }
+
+    return
+     <div>
+{imageListContent}
+
+    </div>;
+  }
 }
 
-ImageResult.propTypes={
-    images:PropTypes.array.isRequired
-}
+ImageResult.propTypes = {
+  images: PropTypes.array.isRequired,
+};
 
 export default ImageResult;
