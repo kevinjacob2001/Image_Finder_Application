8253d4acaf77b66fc675850bd6c89a918f0b4d0c
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-02 11:42:14 +0530

    added a class based component to ImageResults.jsx

---
+++ src/Components/ImageResult/ImageResult.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+class ImageResult extends Component{
+    render(){
+        return(
+Image 
+        )
+    }
+}
+
+export default ImageResult;
