c3614c0c7286073f9c7c588d7653907db7e72549
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-02 12:13:58 +0530

    displayed the images

--- src/Components/ImageResult/ImageResult.jsx
+++ src/Components/ImageResult/ImageResult.jsx
@@ -3,7 +3,7 @@ import { Component } from "react";
 import PropTypes from "prop-types";
 import { GridList, GridTitle } from "material-ui/GridList";
 import IconButton from "material-ui/IconButton";
-import ZoomIn from "material-ui/svg-icon/action/zoom-in";
+import ZoomIn from 'material-ui/svg-icons/action/zoom-in'
 import Dialog from "material-ui/Dialog";
 import FlatButton from "material-ui/FlatButton";
 import GridTile from "material-ui/GridList/GridTile";
@@ -16,38 +16,34 @@ class ImageResult extends Component {
     if (images) {
       imageListContent = (
         <GridList cols={3}>
-          {images.map((img) => {
+          {images.map((img) => (
             <GridTile
-            title={img.tags}
-            key={img.id}
-            subtitle={
+              title={img.tags}
+              key={img.id}
+              subtitle={
                 <span>
-                    by <strong>
-                        {img.user}
-                    </strong>
+                  by <strong>{img.user}</strong>
                 </span>
-            }
-            actionIcon={
+              }
+              actionIcon={
                 <IconButton>
-                    <ZoomIn color="white"/>
+                  <ZoomIn color="white" />
                 </IconButton>
-            }
+              }
             >
-<img src={img.largeImageURL} alt=""/>
-            </GridTile>;
-          })}
+              <img src={img.largeImageURL} alt="" />
+            </GridTile>
+          ))}
         </GridList>
       );
     } else {
       imageListContent = null;
     }
 
-    return
-     <div>
-{imageListContent}
-
-    </div>;
-  }
+    return(
+    <div>{imageListContent}</div>
+    )  
+}
 }
 
 ImageResult.propTypes = {
